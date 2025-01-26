4e7eb6156826ceb56be261ade72be0c8dbf4c18a
Joshua Emery <102991550+EmeryAtRTC@users.noreply.github.com>
2025-01-26 07:48:33 -0800

    Completed part 5

---
+++ app/ui/fonts.ts
@@ -0,0 +1,9 @@
+import { Inter } from "next/font/google";
+import { Roboto } from "next/font/google";
+import { Lusitana } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+
+export const robot = Roboto({ weight: "500" });
+
+export const lusitana = Lusitana({ weight: "400" });
