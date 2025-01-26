3aaaa18aa12ac5b00ba0eac50a6dd8a160e81af2
Joshua Emery <102991550+EmeryAtRTC@users.noreply.github.com>
2025-01-26 08:01:53 -0800

    Updated

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -4,6 +4,6 @@ import { Lusitana } from "next/font/google";
 
 export const inter = Inter({ subsets: ["latin"] });
 
-export const robot = Roboto({ weight: "500" });
+export const robot = Roboto({ weight: "500", subsets: ["latin"] });
 
-export const lusitana = Lusitana({ weight: "400" });
+export const lusitana = Lusitana({ weight: "400", subsets: ["latin"] });
